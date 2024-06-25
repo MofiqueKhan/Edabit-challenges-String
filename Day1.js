d9d8f8b52d415a5a0f22813ae3efe1ab740c20bd
MofiqueKhan <khanmofique@gmail.com>
2024-06-25 18:01:22 +0530

    Edabit-challenges for string

---
+++ Day1.js
@@ -0,0 +1,16 @@
+// 1.
+
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+// Examples
+function giveMeSomething(str){
+    // return "something" + " " + str
+    return `something ${str}`
+
+}
+// console.log( giveMeSomething("is better than nothing") , "something is better than nothing");
+
+// console.log( giveMeSomething("Bob Jane") , "something Bob Jane");
+
+// console.log( giveMeSomething("something") , "something something");
+
