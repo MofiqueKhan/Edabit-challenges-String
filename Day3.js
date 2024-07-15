9b627a0bd11980af32d8a55ebd2e7605bb2aaef4
MofiqueKhan <khanmofique@gmail.com>
2024-07-16 05:09:39 +0530

    edabit challenges

--- Day3.js
+++ Day3.js
@@ -34,10 +34,9 @@ function frontThree(str) {
   //   }
   let result = "";
   for (let i = 0; i <= 2; i++) {
-    for(let j = 0 ; j <=2 ; j++)
-    result += str[j];
+    for (let j = 0; j <= 2; j++) result += str[j];
   }
-  return result
+  return result;
 }
 // console.log(frontThree("Python"), "PytPytPyt");
 
@@ -45,3 +44,144 @@ function frontThree(str) {
 
 // console.log(frontThree("bioshock"), "biobiobio");
 
+// 23.
+
+// Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
+
+// name
+// population
+// continent
+// The string should have the following format: X has a population of Y and is situated in Z (where X is the city name, Y is the population and Z is the continent the city is situated in).
+
+// Examples
+function cityFacts(obj) {
+  return `${obj.name} has a population of ${obj.population} and it situated in ${obj.continent}`;
+}
+// console.log( cityFacts({
+//   name: "Paris",
+//   population: "2,140,526",
+//   continent: "Europe"
+// }) , "Paris has a population of 2,140,526 and is situated in Europe");
+
+// console.log( cityFacts({
+//   name: "Tokyo",
+//   population: "13,929,286",
+//   continent: "Asia"
+// }) , "Tokyo has a population of 13,929,286 and is situated in Asia");
+
+// 24.
+// Create a function that takes in a word and determines whether or not it is plural. A plural word is one that ends in "s".
+
+// Examples
+function isPlural(str) {
+  return str.endsWith("s");
+}
+// console.log( isPlural("changes") , true);
+
+// console.log( isPlural("change") , false);
+
+// console.log( isPlural("dudes") , true);
+
+// console.log( isPlural("magic") , false);
+
+// 25.
+// Create a function that takes a string and returns the concatenated first and last character.
+
+// Examples
+function firstLast(str) {
+  // return str.slice(0,1) + str.slice(-1)
+  return str[0] + str[str.length - 1];
+}
+// console.log( firstLast("ganesh") , "gh");
+
+// console.log( firstLast("kali") , "ki");
+
+// console.log( firstLast("shiva") , "sa");
+
+// console.log( firstLast("vishnu") , "vu");
+
+// console.log( firstLast("durga") , "da");
+
+// 26.
+// Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ... and space after each, and then the word is pronounced with a question mark ?.
+
+// Examples
+function stutter(str) {
+  // return `${str.slice(0,2)}... ${str.slice(0,2)}... ${str}?`;
+  let result = "";
+  for (let i = 0; i < 2; i++) {
+    result += str[i];
+  }
+  return `${result}... ${result}... ${str}?`;
+}
+// console.log(stutter("amazing") , "am... am... amazing?");
+// console.log(stutter("excellent") , "ex... ex... excellent?");
+// console.log(stutter("fantastic") , "fa... fa... fantastic?");
+// console.log( stutter("incredible") , "in... in... incredible?");
+// console.log( stutter("enthusiastic") , "en... en... enthusiastic?");
+// console.log( stutter("outstanding") , "ou... ou... outstanding?");
+
+// 27.
+// Create a function that takes as a parameter an array of "stringified" numbers and returns an array of numbers.
+
+// Example:
+
+// console.log( // ["1", "3", "3.6"] , [1, 3, 3.6]);
+// Examples
+function toNumberArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(+arr[i]);
+  }
+  return result;
+}
+// console.log( toNumberArray(["1", "2", "3"]) , [1, 2, 3]);
+
+// console.log( toNumberArray(["9.4", "4.2"]) , [9.4, 4.2]);
+
+// console.log( toNumberArray(["91", "44"]) , [91, 44]);
+
+// console.log( toNumberArray(["9.5", "8.8"]) , [9.5, 8.8]);
+
+// 28.
+// Given a fraction as a string, return whether or not it is greater than 1 when evaluated.
+
+// Examples
+function greaterThanOne(str) {
+  let result = eval(str);
+  if (result > 1) {
+    return true;
+  }
+  return false;
+}
+// console.log( greaterThanOne("1/2") , false);
+
+// console.log( greaterThanOne("7/4") , true);
+
+// console.log( greaterThanOne("10/10") , false);
+
+//29.
+// Create a function that takes in a current mood and return a sentence in the following format: "Today, I am feeling {mood}". However, if no argument is passed, return "Today, I am feeling neutral".
+
+// Examples
+function moodToday(str = "neutral") {
+  return `Today, I am feeling ${str}`;
+}
+// console.log( moodToday("happy") , "Today, I am feeling happy");
+
+// console.log( moodToday("sad") , "Today, I am feeling sad");
+
+// console.log( moodToday() , "Today, I am feeling neutral");
+
+// 30.
+// Create a function that returns the number of syllables in a simple string. The string is made up of short repeated words like "Lalalalalalala" (which would have 7 syllables).
+
+// Examples
+function countSyllables(str){
+  // return str.length / 2
+}
+console.log(countSyllables("Hehehehehehe"), 6);
+
+console.log(countSyllables("bobobobobobobobo"), 8);
+
+console.log(countSyllables("NANANA"), 3);
